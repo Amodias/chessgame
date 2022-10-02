ba29bd4d13a23be4a06de51eb26166386b33ece6
amodias <saidamokranebelgacem@gmail.com>
2022-10-02 08:01:44 +0100

    Socket configuration added

--- server.js
+++ server.js
@@ -2,33 +2,43 @@ var express = require('express'); // Get the module
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const crypto = require("crypto");
+const randomId = () => crypto.randomBytes(8).toString("hex");
 app.use(express.static(__dirname));
 app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html');;
 });
 
-var clients = [];
 
-//Whenever someone connects this gets executed
+
+users = {};
+
 io.on('connection', function(socket){
    console.log('A user connected');
+   socket.userID = randomId();
+   socket.username = socket.handshake.query.username; 
+   users[socket.userID] = {userID : socket.userID ,username : socket.username ,socketID :socket.id}; 
+   console.log(users);
+   socket.emit("users", users);
+   socket.broadcast.emit("user-connected", {
+      userID: socket.userID,
+      username: socket.username,
+      connected: true,
+      messages: [],
+    });
    
-   io.sockets.on('connect', function(client) {
-      clients.push(client);
+
   
-      client.on('disconnect', function() {
-          clients.splice(clients.indexOf(client), 1);
-      });
-  });
-   
 
-
-   //Whenever someone disconnects this piece of code executed
    socket.on('disconnect', function () {
-      clients.splice(clients.indexOf(client), 1);
+      console.log(users);
+      delete users[socket.userID];
+      socket.broadcast.emit("user-disconnected", {
+         userID: socket.userID,
+         username: socket.username,
+         connected: false,
+       });
       console.log('A user disconnected');
    });
-
-   console.log(clients);
 
 });
 http.listen(3000, function(){
