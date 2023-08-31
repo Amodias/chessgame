f0a2c6b528e23a8fb4227bbed2abe87fe006716a
amodias <saidamokranebelgacem@gmail.com>
2023-08-31 17:56:46 +0100

    remove cosnole.log

--- client/src/services/pawn-actions/index.js
+++ client/src/services/pawn-actions/index.js
@@ -3,20 +3,12 @@ export const movePawn = (chess, from, to) => {
     console.log(chess);
     convertNotationToPosition(to);
     const move = chess.move({ from, to });
-    if (move) {
-      console.log('Valid move');
-      
-    } else {
-      console.log('Invalid move');
-    }
     return move;
   };
 
  export const getPossibleMoves = (chess, from ) => {
     let possibleMoves = chess.moves({ square: from });
-    console.log(`Possible moves for :`, possibleMoves);
     possibleMoves = convertNotationToPosition(possibleMoves);
-    console.log(`Possible moves for :`, possibleMoves);
     return possibleMoves ;
 
   };
