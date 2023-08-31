d2036605c006da09ba23aef1568966a9143625aa
amodias <saidamokranebelgacem@gmail.com>
2023-08-31 21:28:54 +0100

    set the new multi player compnents routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './styles/App.css';
 import Single from './pages/playMode/single';
 import Ia from './pages/playMode/ia';
-import Multi from './pages/playMode/multi';
+import MultiPlayer from './pages/playMode/multi-player/index.tsx';
 import Login from './pages/authentification/Login';
 import Register from './pages/authentification/Register';
 import Home from './pages/Home';
@@ -20,7 +20,7 @@ function App() {
     
           <Route path="/" element={<ProtectedRoute element={Home} />} />
           <Route path="/single-player" element={<ProtectedRoute element={Single} />} />
-          <Route path="/multi-player" element={<ProtectedRoute element={Multi} />} />
+          <Route path="/multi-player" element={<ProtectedRoute element={MultiPlayer} />} />
           <Route path="/vs-ia" element={<ProtectedRoute element={Ia} />} />
 
         </Routes>
