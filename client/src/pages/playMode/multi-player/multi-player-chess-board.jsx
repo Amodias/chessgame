78463fc7fa2ef1aff160c3e5357791d76719ce61
amodias <saidamokranebelgacem@gmail.com>
2023-08-31 22:14:04 +0100

    create the multi chess board

---
+++ client/src/pages/playMode/multi-player/multi-player-chess-board.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ChessBoard from "../../../components/chessboard";
+
+const MultiPlayerChessBoard = () => {
+  
+
+  return <ChessBoard  />;
+};
+export default MultiPlayerChessBoard;
