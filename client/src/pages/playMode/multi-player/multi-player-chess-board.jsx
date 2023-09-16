fa2a40ba6c429a4251454d9d72dd830bbb017cad
amodias <saidamokranebelgacem@gmail.com>
2023-09-16 17:10:51 +0100

    clean code

--- client/src/pages/playMode/multi-player/multi-player-chess-board.jsx
+++ client/src/pages/playMode/multi-player/multi-player-chess-board.jsx
@@ -1,7 +1,5 @@
 import React , {useState , useEffect} from "react";
 import ChessBoard from "../../../components/chessboard";
-import { Chess } from 'chess.js';
-import socketService from "../../../services/socketServices";
 
 
 const MultiPlayerChessBoard = () => {
