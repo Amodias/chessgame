df7a1b7e0a6f8cfcc2120dd3e844d329273b4bf1
amodias <saidamokranebelgacem@gmail.com>
2023-12-08 13:16:04 +0100

    fix npm dependecies & init stockfish module

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import './styles/App.css';
-import Single from './pages/playMode/single';
-import Ia from './pages/playMode/ia';
-import MultiPlayer from './pages/playMode/multi-player/index.tsx';
-import Login from './pages/authentification/Login';
-import Register from './pages/authentification/Register';
-import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProtectedRoute from './components/routes/ProtectedRoute';
-import Logout from './pages/authentification/Logout';
+import React from "react";
+import "./styles/App.css";
+import Single from "./pages/playMode/single";
+import Ia from "./pages/playMode/ia";
+import MultiPlayer from "./pages/playMode/multi-player/index.jsx";
+import Login from "./pages/authentification/Login";
+import Register from "./pages/authentification/Register";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/routes/ProtectedRoute";
+import Logout from "./pages/authentification/Logout";
 
 function App() {
   return (
@@ -17,12 +18,17 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/logout" element={<Logout />} />
-    
-          <Route path="/" element={<ProtectedRoute element={Home} />} />
-          <Route path="/single-player" element={<ProtectedRoute element={Single} />} />
-          <Route path="/multi-player" element={<ProtectedRoute element={MultiPlayer} />} />
-          <Route path="/vs-ia" element={<ProtectedRoute element={Ia} />} />
 
+          <Route path="/" element={<ProtectedRoute element={Home} />} />
+          <Route
+            path="/single-player"
+            element={<ProtectedRoute element={Single} />}
+          />
+          <Route
+            path="/multi-player"
+            element={<ProtectedRoute element={MultiPlayer} />}
+          />
+          <Route path="/vs-ia" element={<ProtectedRoute element={Ia} />} />
         </Routes>
       </Router>
     </div>
